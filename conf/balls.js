6cbb952755573decb3bd89c02fc9383022b80d1f
idsbllp <idsbllp@gmail.com>
2017-08-15 15:12:09 +0800

    feat: add手指拖动效果

--- conf/balls.js
+++ conf/balls.js
@@ -1,17 +1,17 @@
 const pics = ['asd.jpg', 'q.jpg', 'bg.jpg', 'bl.png', 'dd.jpg']
 
 function getPos() {
-    let perimeter = Math.PI * 2;
-    let per = perimeter / 5;
+    let perimeter = Math.PI * 2
+    let per = perimeter / 5
 
-    let pos = [];
+    let pos = []
     for (var i = 5; i > 0; i--) {
         pos.push({
             pic: pics[i-1],
             pos: [100 * Math.cos(per*i), 0, 100 * Math.sin(per*i)]
         })
     }
-    return pos;
+    return pos
 }
 
 export default getPos()
--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 
 import 'three-onevent'
 import positionOfBalls from './conf/balls.js'
+import OrbitControls from 'three-orbitcontrols'
 import { RAF, CRAF } from './utils/index.js'
 
 const { PI, cos, sin, random } = Math
@@ -27,14 +28,28 @@ let timer = null
 
 
 function init() {
-    container = document.createElement( 'div' )
-    document.body.appendChild( container )
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 )
+    container = document.getElementById( 'container' )
+    // document.body.appendChild( container )
 
-    camera.position.y = 400
     scene = new THREE.Scene()
-    // scene.add(WebVR.Camera)
+
+    renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } )
+    renderer.setPixelRatio( window.devicePixelRatio )
+    renderer.setSize( window.innerWidth, window.innerHeight )
+    renderer.setClearColor(0xffffff, 0)
+
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 )
+    camera.position.x = 300
+    camera.position.y = 300
+    camera.position.z = 300
+
+    // 给小球添加点击事件
     THREE.onEvent(scene, camera)
+
+    // 给屏幕添加手指拖动事件
+    controls = new OrbitControls( camera, renderer.domElement );
+    controls.addEventListener( 'change', render );
+    controls.enableZoom = false;
 
     // 开灯 亮度
     scene.add( new THREE.AmbientLight( 0x404040 ) )
@@ -57,26 +72,20 @@ function init() {
         object = new THREE.Mesh( new THREE.SphereGeometry( 15, 75, 75 ), material )
         // 左右, 上下, 斜的？？？？
         object.position.set( value.pos[0], value.pos[1], value.pos[2] )
-        object.name = value.pic
+        object.name = `${value.pic}_ball`
         object.on('click', function(e) {
             console.log(`你点击了第${index}个球： ${value.pic}`)
             showDetail(index)
         })
+        circle = new THREE.Mesh( new THREE.TorusGeometry(30, 1, 5, 60), material )
+        circle.position.set( 0,0,0, )
+        circle.name = `${value.pic}_circle`
+        object.add( circle )
         scene.add( object )
-        // controls = new THREE.OrbitControls( object )
 
         // 球周围的圆
-        circle = new THREE.Mesh( new THREE.TorusGeometry(30, 1, 5, 60), material )
-        circle.position.set( value.pos[0], value.pos[1], value.pos[2] )
-        scene.add( circle )
 
     })
-
-    renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } )
-    renderer.setPixelRatio( window.devicePixelRatio )
-    renderer.setSize( window.innerWidth, window.innerHeight )
-
-    renderer.setClearColor(0xffffff, 0)
 
     container.appendChild( renderer.domElement )
 
@@ -99,8 +108,9 @@ function showDetail(index = 5) {
 }
 function animate() {
     timer = RAF(animate)
+    controls.update()
     stats.update()
-    moveBody()
+    // moveBody()
     render()
 }
 
@@ -118,10 +128,14 @@ const startPos = {
 let cameraRotate = 0
 // http://www.cnblogs.com/v-weiwang/p/6072235.html
 function render() {
-    let rotate = 0.03
-    camera.position.x = cameraPos.x
-    camera.position.y = cameraPos.y
-    camera.position.z = cameraPos.z
+    // let rotate = 0.004
+    // 1 / 360 /2 2 (0.5度)
+    let rotate = 0.0028 / 2
+    let perimeter = Math.PI * 2
+    let per = perimeter / 5
+    // camera.position.x = cameraPos.x
+    // camera.position.y = cameraPos.y
+    // camera.position.z = cameraPos.z
     // controls.update();
     camera.lookAt({ x:0, y:0, z:0 })
 
@@ -131,12 +145,24 @@ function render() {
         object.rotation.x += rotate
         object.rotation.y += rotate
         object.rotation.z += rotate
+        // 公转
+        if (object.name && test < 20) {
+            // test++
+            // console.log(object.position.x)
+            object.position.x = 100 * cos(object.rotation.x+per*i)
+            object.position.z = 100 * sin(object.rotation.x+per*i)
+        }
 
-        if (test < 20 && i == 3) {
+        if (test < 4) {
             test++
+            // console.log(scene.children)
             // console.log(object.position, camera.position)
         }
     }
+    // positionOfBalls.forEach((value, index) => {
+    //     object.position.x = 100 * cos(Math.PI * 2.5*i)
+    //     object.position.z = 100 * sin(Math.PI * 2.5*i)
+    // })
     // 自行转动，绕y轴转动，y轴垂直向上
     cameraRotate -= 0.005
     // cameraPos.x = 300 * cos(cameraRotate)
@@ -158,30 +184,31 @@ function moveBody(rotation = { clientX: 0, clientY: cameraPos.y }) {
     renderer.render( scene, camera )
 }
 
-window.addEventListener('touchstart', e => {
-    // CRAF(timer)
-    let { clientX, clientY } = e.changedTouches[0]
-    if (clientX < 0 || clientY < 0 || clientX > window.innerWidth || clientY > window.innerHeight) {
-        return
-    }
-    startPos.x = clientX
-    startPos.y = clientY
-})
+// window.addEventListener('touchstart', e => {
+//     // CRAF(timer)
+//     let { clientX, clientY } = e.changedTouches[0]
+//     if (clientX < 0 || clientY < 0 || clientX > window.innerWidth || clientY > window.innerHeight) {
+//         return
+//     }
+//     startPos.x = clientX
+//     startPos.y = clientY
+// })
 
-window.addEventListener('touchmove', e => {
-    let { clientX, clientY } = e.changedTouches[0]
-    if (clientX < 0 || clientY < 0 || clientX > window.innerWidth || clientY > window.innerHeight) {
-        return
-    }
+// window.addEventListener('touchmove', e => {
+//     let { clientX, clientY } = e.changedTouches[0]
+//     if (clientX < 0 || clientY < 0 || clientX > window.innerWidth || clientY > window.innerHeight) {
+//         return
+//     }
 
-    clientX -= startPos.x
-    clientY -= startPos.y
+//     clientX -= startPos.x
+//     clientY -= startPos.y
 
-    // moveBody({ clientX, clientY })
+//     // moveBody({ clientX, clientY })
 
-})
+// })
 
 init()
+render();
 animate()
 
 export default 'llp'
